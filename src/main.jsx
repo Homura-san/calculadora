3e605a5a8e2cd2e93369cde73287dcab5ad56ece
Homura-san <erickbispo@ufba.br>
2025-10-02 23:20:22 -0300

    Componente Calculator

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Calculator from './main/Calculator'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
+    
   </StrictMode>,
 )
